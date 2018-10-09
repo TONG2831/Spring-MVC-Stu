2dba3cfec7aa9c07cf54eec9e13a2c3b6d69ae94
liutong <1352255170@qq.com>
2018-10-09 18:18:09 +0800

    注释@RequestMapping在类处和方法处的区别

--- springmvc/src/com/spring/controller/SpringMVCTest.java
+++ springmvc/src/com/spring/controller/SpringMVCTest.java
@@ -8,7 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SpringMVCTest {
 	
 	private static String SUCCESS = "success";
-	
+	/**
+	 * 1.@RequestMapping 可以修饰方法,也可以修饰类;
+	 * 2.
+	 * 1).在类上,提供初步的请求映射信息.相对于WEB项目的根路径
+	 * 2).在方法上,提供进一步的细分映射信息.相对于类定义出标记的的路径
+	 * 如果类定义处未标注@RequestMapping,则在方法处标记的URL,相对于WEB应用的根路径.
+	 * @return
+	 */
 	@RequestMapping("/test")
 	public String helloTest() {
 		System.out.println("test spring mvc");
